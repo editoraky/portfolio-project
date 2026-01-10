05efee02bc3c2a4b5e316d4584919aef99f5de90
editoraky <akin@femka.de>
2026-01-10 11:03:06 +0100

    feat: add portfolio section with project images

---
+++ src/app/sections/portfolio/portfolio.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit, OnDestroy, HostListener } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-portfolio',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './portfolio.html',
+  styleUrl: './portfolio.scss'
+})
+export class PortfolioComponent implements OnInit, OnDestroy {
+  
+  isTitleGreen = false;
+  isMobile = false;
+  
+  private titleInterval: any;
+
+  ngOnInit() {
+    this.checkScreenSize();
+  }
+
+  ngOnDestroy() {
+    if (this.titleInterval) {
+      clearInterval(this.titleInterval);
+    }
+  }
+
+  @HostListener('window:resize')
+  onResize() {
+    this.checkScreenSize();
+  }
+
+  checkScreenSize() {
+    const wasMobile = this.isMobile;
+    this.isMobile = window.innerWidth <= 768;
+
+    if (this.isMobile && !wasMobile) {
+      this.startTitleAnimation();
+    }
+    
+    if (!this.isMobile && wasMobile) {
+      this.stopTitleAnimation();
+    }
+
+    if (this.isMobile && !this.titleInterval) {
+      this.startTitleAnimation();
+    }
+  }
+
+  startTitleAnimation() {
+    this.isTitleGreen = true;
+
+    this.titleInterval = setInterval(() => {
+      if (this.isTitleGreen) {
+        this.isTitleGreen = false;
+        setTimeout(() => {
+          this.isTitleGreen = true;
+        }, 1000);
+      }
+    }, 2500);
+  }
+
+  stopTitleAnimation() {
+    if (this.titleInterval) {
+      clearInterval(this.titleInterval);
+      this.titleInterval = null;
+    }
+    this.isTitleGreen = false;
+  }
+}
